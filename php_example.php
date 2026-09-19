59cc3a27ad3bdc8211fbcb0b8bc4805410aa9f06
Lucas <l328f965@ku.edu>
2026-09-18 21:46:03 -0500

    Last, last late night changes

--- php_example.php
+++ php_example.php
@@ -8,13 +8,15 @@
 <?php
 echo '<table>';
 
+//this little recursive check is because the form submits to itself
 if ($_GET["number"] != ""){
     $range = (int) $_GET["number"];
-    echo '<tr>';
-    echo '<th>&nbsp;</th>';
     echo '<tbody>';
     //first echo the table top values
-    for ($i=1; $i != $range+1; $i++){
+    for ($i=0; $i != $range+1; $i++){
+        //tao forgive me this is some bs but i wanna go out its a friday night
+        if ($i == 0) {echo '<th> &nbsp;</th>'; continue;}
+        //added the correct spacing, now we do this
         echo '<th>' . $i . '</th>';
     }
     echo '</tr>';
@@ -22,11 +24,15 @@ if ($_GET["number"] != ""){
     //in the range between 1 to $range ($a) and then mult between 1 to range ($a * $b)
     for ($a=1; $a != $range+1; $a++){
         echo '<td>';
+        //make a new talbe
         echo '<th>' . $a . '</th>';
+        //iterate through a range with our chosen y column value
         for ($b = 1; $b != $range+1; $b++){
+            //now calculate the x*y value and echo it
             $c = $a * $b;
             echo '<td>' . $c . '</td>';
         }
+        //break otu
         echo '</tr>';
     }
 
